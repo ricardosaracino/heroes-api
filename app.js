7f84fcd7005602a8e6304a0397034dea9ca2c8e1
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2018-04-27 15:52:56 -0400

    router clean up

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', heroesRouter);
+app.use('/heroes', heroesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/heroes.js
+++ routes/heroes.js
@@ -1,36 +1,120 @@
 var express = require('express');
 var router = express.Router();
 
-//http://github.com/felixge/node-couchdb
-var couchdb = require('../node_modules/felix-couchdb/lib/couchdb'),
-	client = couchdb.createClient(5984, 'localhost'),
-	db = client.db('heroes');
+
+var nano = require('nano')('http://localhost:5984'),
+	db = nano.use('heroes');
 
 
-router.get('/heroes', function (request, res, next) {
-	db.allDocs({'type': 'heroes'}, function (error, doc) {
+
+
+
+function requireRole (role) {
+
+	return function (req, res, next) {
+		if (req.session && req.session.user && req.session.user.role === role) {
+			next();
+		} else {
+			res.send(403);
+		}
+	}
+}
+
+
+
+/*
+http://...com/posts/create- POST request  -> Goes to posts.create() method in the server
+http://...com/posts/1/show- GET request  -> Goes to posts.show(1) method in the server
+http://...com/posts/1/delete - POST request  -> Goes to posts.delete(1) method in the server
+http://...com/posts/1/edit- POST request  -> Goes to posts.edit(1) method in the server
+
+
+http://...com/posts - POST request  -> Goes to posts.create() method in the server
+http://...com/posts/1 - GET request  -> Goes to posts.show(1) method in the server
+http://...com/posts/1 - DELETE request  -> Goes to posts.delete(1) method in the server
+http://...com/posts/1 - PUT request  -> Goes to posts.edit(1) method in the server
+
+
+http://...com/posts - GET request  -> Goes to posts.all() method in the server
+http://...com/posts/1 - GET request  -> Goes to posts.show(1) method in the server
+
+
+http://...com/posts - POST request  -> Goes to posts.create() method in the server
+http://...com/posts - POST request  -> Goes to posts.edit() method in the server
+http://...com/posts/1 - POST request  -> Goes to posts.delete(1) method in the server
+*/
+
+
+// https://expressjs.com/en/4x/api.html
+
+
+
+
+
+router.get('/', function (request, res, next) {
+
+	var q = {
+		selector: {
+			type: {
+				"$eq": "hero"
+			}
+		}
+	};
+
+	db.find(q, function (error, body) {
 		if (error) {
 			res.write(JSON.stringify(error));
 		}
 		else {
-			res.write(JSON.stringify(doc));
+			res.write(JSON.stringify(body));
 		}
-
 		res.end();
 	});
 });
 
 
-router.get('/hero/:id', function (request, res, next) {
 
-	db.getDoc(request.params.id, function (error, doc) {
+router.get('/:id', function (request, res) {
+	db.get(request.params.id, function (error, body) {
 		if (error) {
 			res.write(JSON.stringify(error));
 		}
 		else {
-			res.write(JSON.stringify(doc));
+			res.write(JSON.stringify(body));
 		}
+		res.end();
+	});
+});
 
+
+
+router.post('/', requireRole('user'), function (request, res) {
+
+	request.body.type = 'hero';
+
+	db.insert(request.body, function (error, body) {
+		if (error) {
+			res.write(JSON.stringify(error));
+		}
+		else {
+			res.write(JSON.stringify(body));
+		}
+		res.end();
+	});
+});
+
+
+router.post('/:id', function (request, res) {
+
+	request.body.type = "hero";
+
+	db.insert(request.body, function (error, body) {
+		if (error) {
+			res.write(JSON.stringify(error));
+		}
+		else {
+			res.write(JSON.stringify(body));
+		}
 		res.end();
 	});
 });
