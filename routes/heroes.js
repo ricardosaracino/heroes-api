2f4b30478fb28a0b41db9a4333cee0d59048c077
Ricardo Saracino <ricardo.saracino@ricardosaracino.com>
2018-04-28 21:55:57 -0400

    added 404 responses, added listen, removed render

--- routes/heroes.js
+++ routes/heroes.js
@@ -2,14 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
+// https://github.com/apache/nano#dbdestroydocname-rev-callback
 var nano = require('nano')('http://localhost:5984'),
 	db = nano.use('heroes');
 
 
-
-
-
-function requireRole (role) {
+function requireRole(role) {
 
 	return function (req, res, next) {
 		if (req.session && req.session.user && req.session.user.role === role) {
@@ -19,7 +17,6 @@ function requireRole (role) {
 		}
 	}
 }
-
 
 
 /*
@@ -44,12 +41,7 @@ http://...com/posts - POST request  -> Goes to posts.edit() method in the server
 http://...com/posts/1 - POST request  -> Goes to posts.delete(1) method in the server
 */
 
-
 // https://expressjs.com/en/4x/api.html
-
-
-
-
 
 router.get('/', function (request, res, next) {
 
@@ -63,20 +55,20 @@ router.get('/', function (request, res, next) {
 
 	db.find(q, function (error, body) {
 		if (error) {
+			res.status(404);
 			res.write(JSON.stringify(error));
 		}
 		else {
-			res.write(JSON.stringify(body));
+			res.write(JSON.stringify(body.docs));
 		}
 		res.end();
 	});
 });
-
-
 
 router.get('/:id', function (request, res) {
 	db.get(request.params.id, function (error, body) {
 		if (error) {
+			res.status(404);
 			res.write(JSON.stringify(error));
 		}
 		else {
@@ -86,14 +78,64 @@ router.get('/:id', function (request, res) {
 	});
 });
 
-
-
-router.post('/', requireRole('user'), function (request, res) {
+router.post('/', function (request, res) {
 
 	request.body.type = 'hero';
+	request.body.created = new Date().getTime();
 
 	db.insert(request.body, function (error, body) {
 		if (error) {
+			res.status(404);
+			res.write(JSON.stringify(error));
+			res.end();
+		}
+		else {
+			// body contains id, rev, ok
+			db.get(body.id, function (error, body) {
+				if (error) {
+					res.status(404);
+					res.write(JSON.stringify(error));
+				}
+				else {
+					res.write(JSON.stringify(body));
+				}
+				res.end();
+			});
+		}
+	});
+});
+
+router.put('/', function (request, res) {
+
+	request.body.type = 'hero';
+	request.body.updated = new Date().getTime();
+
+	db.insert(request.body, function (error, body) {
+		if (error) {
+			res.status(404);
+			res.write(JSON.stringify(error));
+			res.end();
+		}
+		else {
+			// body contains id, rev, ok
+			db.get(body.id, function (error, body) {
+				if (error) {
+					res.status(404);
+					res.write(JSON.stringify(error));
+				}
+				else {
+					res.write(JSON.stringify(body));
+				}
+				res.end();
+			})
+		}
+	});
+});
+
+router.delete('/', function (request, res) {
+	db.destroy(request.body._id, request.body._rev, function (error, body) {
+		if (error) {
+			res.status(404);
 			res.write(JSON.stringify(error));
 		}
 		else {
@@ -102,22 +144,5 @@ router.post('/', requireRole('user'), function (request, res) {
 		res.end();
 	});
 });
-
-
-router.post('/:id', function (request, res) {
-
-	request.body.type = "hero";
-
-	db.insert(request.body, function (error, body) {
-		if (error) {
-			res.write(JSON.stringify(error));
-		}
-		else {
-			res.write(JSON.stringify(body));
-		}
-		res.end();
-	});
-});
-
 
 module.exports = router;
